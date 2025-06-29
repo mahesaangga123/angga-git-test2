22b25e34747f276488b4ddf4b24b1b2d7d6fa42c
Angga Restu <mahesaangga900@gmail.com>
2025-06-29 16:48:04 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia atque molestias voluptatibus eum laudantium, iure veniam placeat in accusantium quia! Consectetur, quam aperiam? Libero voluptas eius quas corporis laborum enim?</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 </html>
